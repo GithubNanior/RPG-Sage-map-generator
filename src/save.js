2bac795abb520853110ca77a40e2f7970962917c
Nanior <jakob201611@yahoo.com>
2025-06-25 19:52:17 +0300

    Feat: Add a save module, slightly abstracting the localStorage's interface to make switching tools easy

---
+++ src/save.js
@@ -0,0 +1,45 @@
+function canSave()
+{
+    try {
+        const x = "__storage_test__";
+        window.localStorage.setItem(x, x);
+        window.localStorage.removeItem(x);
+        return true;
+    } catch (e) {
+        return (e instanceof DOMException && e.name === "QuotaExceededError" && window.localStorage && window.localStorage.length !== 0);
+    }
+}
+
+function saveCount()
+{
+    return window.localStorage.length;
+}
+
+function getKey(index)
+{
+    return window.localStorage.key(index);
+}
+
+function getValue(key)
+{
+    return window.localStorage.getItem(key);
+}
+
+function save(key, value)
+{
+    window.localStorage.setItem(key, value);
+}
+
+function deleteSave(key)
+{
+    window.localStorage.removeItem(key);
+}
+
+export {
+    canSave,
+    saveCount,
+    getKey,
+    getValue,
+    save,
+    deleteSave
+};
